c7e2a0dffbc202a314c21555e8c5812507b70ef6
megane0109 <megane0109@gmai.com>
2026-04-12 04:47:20 +0900

    Development

---
+++ index.js
@@ -0,0 +1,19 @@
+ async function logToDiscord() {
+        // Webhook URL
+        const webhookUrl = 'https://discordapp.com/api/webhooks/1492542194870128850/mInnnsxcHerVFC6AP-AtPMclcmADVoj8fjQIirC61lXD32eGWzgVrkNH_8kBvzircBPw';
+
+        // 投稿内容
+        const payload = {
+            "content": "# Hello from Webhook"
+        };
+
+        try {
+            await fetch(webhookUrl, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(payload)
+            });
+        } catch (error) {
+            console.error('Discordへの送信に失敗しました:', error);
+        }
+        }
