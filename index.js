2f45842e8e34f9479910a97b14530c5e3ceb3d58
megane0109 <megane0109@gmail.com>
2026-04-20 08:09:47 +0900

    Development

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ async function bringData() {
 
 
 
-function pushDraw() {
+async function pushDraw() {
   const delivery_day = document.getElementById("calendar").value;
   const delivery_day_as_number = document.getElementById("calendar").valueAsNumber;
   const number_parse_day = new Date(delivery_day_as_number);
@@ -88,6 +88,21 @@ function pushDraw() {
 
   markdown.value = `# ${month}月${date}日${day}__${hour}:${minute}__納品\n## ・\`${order}\`     \`x${box}箱\``;
 
-  console.log(`# ${month}月${date}日${day}__${hour}:${minute}__納品`);
+  const webhookUrl = 'https://discordapp.com/api/webhooks/1492542194870128850/mInnnsxcHerVFC6AP-AtPMclcmADVoj8fjQIirC61lXD32eGWzgVrkNH_8kBvzircBPw';
+
+        // 投稿内容
+        const payload = {
+            "content": "# Hello from Webhook"
+        };
+
+        try {
+            await fetch(webhookUrl, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(payload)
+            });
+        } catch (error) {
+            console.error('Discordへの送信に失敗しました:', error);
+        }
 
 }
